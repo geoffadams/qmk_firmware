ed5073192a7bba101b8ec62364fdb0a358394eb6
Geoff Adams <geoff@dianode.net>
2018-03-02 12:53:10 +0000

    dz60:dianode - Left shift sends KC_ESC on tap

--- keyboards/dz60/keymaps/dianode/keymap.c
+++ keyboards/dz60/keymaps/dianode/keymap.c
@@ -8,6 +8,7 @@
 #define _____ KC_TRNS
 #define XXXXX KC_NO
 
+#define LSFT_ESC MT(MOD_LSFT, KC_ESC)
 #define RSFT_ESC MT(MOD_RSFT, KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -15,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   XXXXX,    KC_BSPC,
 		KC_TAB,             KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
 		MO(_EXT),           KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
-		KC_LSFT,  KC_GRV,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  XXXXX,    RSFT_ESC, KC_MEH,
+		LSFT_ESC, KC_GRV,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  XXXXX,    RSFT_ESC, KC_MEH,
 		KC_LCTL,  KC_LALT,            KC_LGUI,  XXXXX,              KC_SPC,             XXXXX,              KC_RGUI,  MO(_KBD), XXXXX,    MO(_NAV), KC_HYPR
   ),
 
